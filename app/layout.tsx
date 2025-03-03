dd0bde9bdcefbf49fdd2cc784bc7f9091b7ba6e9
Adrián <ardillan@me.com>
2025-03-03 13:31:44 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/global.css";
 
 import React, { JSX } from "react";
+import Script from "next/script";
 
 import ContextProvider from "@/context/ContextProvider";
 
@@ -9,6 +10,12 @@ import RootContent from "./RootContent";
 export default function RootLayout({ children }: { children: JSX.Element }) {
   return (
     <ContextProvider>
+      <Script
+        data-goatcounter="https://ardi.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+        strategy="afterInteractive"
+      />
       <RootContent>{children}</RootContent>
     </ContextProvider>
   );
