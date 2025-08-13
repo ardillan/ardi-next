efa29314255da2036a4319607d41c1e3935deec0
Ardi <ardillan@me.com>
2025-08-13 21:14:02 +0200

    feat: add fonts

---
+++ app/styles/fonts.css.ts
@@ -0,0 +1,15 @@
+import { fontFace } from "@vanilla-extract/css";
+
+export const fontDKParmesi = fontFace([
+  {
+    src: "local('DK Pardesi'), url('/fonts/DK-Pardesi.otf') format('otf')",
+    fontWeight: "normal",
+  },
+]);
+
+export const fontDinish = fontFace([
+  {
+    src: "local('DINish'), url('/fonts/DINish.ttf') format('ttf')",
+    fontWeight: "normal",
+  },
+]);
