3ffa8e92eefbe6b43e3d1b80deab4866a8be63d2
Ardi <ardillan@me.com>
2025-11-08 20:09:51 +0100

    feat: generate static file for Feed

--- src/lib/generate-rss.ts
+++ src/lib/generate-rss.ts
@@ -1,27 +1,24 @@
+import fs from "fs";
+import path from "path";
+
 import { getPostData, getSortedPostsData } from "@/lib/getPostData";
 import { escapeHtml, markdownToHtml, sanitizeString } from "@/lib/helpers";
 
-export async function GET() {
-  const items = await getItems();
-
+async function generateRss() {
+  const items = await getItems(); // tu misma función
   const xml = `<?xml version="1.0" encoding="utf-8"?>
-                <feed xmlns="http://www.w3.org/2005/Atom">
-                  <title>Ardi RSS</title>
-                  <subtitle>Un compendio de artículos, reflexiones y notas.</subtitle>
-                  <link href="https://ardi.monster/" />
-                  <link rel="self" href="https://ardi.monster/rss/feed.xml" />
-                  <id>https://ardi.monster/</id>
-                  <author>
-                    <name>Ardi</name>
-                  </author>
-                  ${items}
-                </feed>`;
-
-  return new Response(xml, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
+  <feed xmlns="http://www.w3.org/2005/Atom">
+    <title>Ardi RSS</title>
+    <subtitle>Un compendio de artículos, reflexiones y notas.</subtitle>
+    <link href="https://ardi.monster/" />
+    <link rel="self" href="https://ardi.monster/rss/feed.xml" />
+    <id>https://ardi.monster/</id>
+    <author><name>Ardi</name></author>
+    ${items}
+  </feed>`;
+  const filePath = path.join(process.cwd(), "public/rss/feed.xml");
+  fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  fs.writeFileSync(filePath, xml);
 }
 
 const getItems = async (): Promise<string> => {
@@ -54,3 +51,5 @@ const getItems = async (): Promise<string> => {
 
   return items.filter(Boolean).join("");
 };
+
+generateRss();
