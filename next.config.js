053e15f8d344d032f884e4aba07f44d0e422fd16
Ardi <ardillan@me.com>
2025-11-10 18:46:16 +0100

    feat: restore config file

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
-const path = require("path");
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
-
 const withVanillaExtract = createVanillaExtractPlugin();
 
-module.exports = withVanillaExtract({
-  output: "standalone",
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-  // Para monorepos: si tienes archivos compartidos fuera de tu app
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-
-  // Archivos extra que sí deben incluirse en las funciones
-  outputFileTracingIncludes: {
-    "/api/github/ardi": ["src/lib/github/**/*"],
-    "/api/notion": ["src/lib/notion/**/*"],
-    "/api/strava": ["src/lib/strava/**/*"],
-    "/api/weather": ["src/lib/weather/**/*"],
-  },
-
-  // Excluimos los posts grandes de public para que no exploten el bundle
-  outputFileTracingExcludes: {
-    "/[slug]": ["public/posts/**/*"],
-    "/blog/[...id]": ["public/posts/**/*"],
-    "/memorias/[...id]": ["public/posts/**/*"],
-    "/api/*": ["public/posts/**/*"],
-  },
-});
+module.exports = withVanillaExtract(nextConfig);
