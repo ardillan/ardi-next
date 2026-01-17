595e12bdc6e61ba43072843908f0009d8468b8a0
Ardi <ardillan@me.com>
2026-01-17 21:57:46 +0100

    feat: update welcome component

--- app/ui/general/Welcome.tsx
+++ app/ui/general/Welcome.tsx
@@ -3,29 +3,24 @@ import React from "react";
 import { Facts } from "@/appComponents/general/Facts";
 import getStravaRides from "@/lib/getStrava";
 import { metersToKM } from "@/lib/helpers";
-import { mainContainer } from "@/styles/common/grid.css";
-import { theme } from "@/styles/common/theme.css";
 
-import { description, introduction, welcomeContainer } from "./Welcome.css";
+import styles from "./Welcome.module.css";
 
 const Welcome = async () => {
   const ridesPerYear = await getStravaRides();
   return (
-    <div className={mainContainer}>
-      <div className={welcomeContainer}>
-        <h1 className={introduction}>
-          <span style={{ color: theme.color.secondary }}>¡Hola!</span>
+    <div className={styles.welcome}>
+      <div>
+        <h1>
+          <span>¡Hola!</span>
           <br />
           <span>¡Soy Ardi!</span>
         </h1>
 
-        <p className={description}>
+        <p>
           <span>
             Tal vez me recuerdes por <Facts /> ó por haber recorrido unos{" "}
-            <strong style={{ color: theme.color.primary }}>
-              {metersToKM(ridesPerYear)}
-            </strong>{" "}
-            km. en bicicleta.
+            <strong>{metersToKM(ridesPerYear)}</strong> km. en bicicleta.
           </span>
         </p>
       </div>
