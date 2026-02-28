6888fc11383be08d2ce72316aea3fdbc05daabe5
Ardi <ardillan@me.com>
2026-02-28 22:43:30 +0100

    feat: remove Vanilla Extract

--- app/ui/layouts/BlogLayout.tsx
+++ app/ui/layouts/BlogLayout.tsx
@@ -5,7 +5,7 @@ import { getSortedPostsData } from "@/lib/getPostData";
 
 import Posts from "../general/Posts";
 import SuperMarkdown from "../general/SuperMarkdown";
-import { header, headerSubtitle, headerTitle } from "./BlogLayout.css";
+import styles from "./BlogLayout.module.css";
 
 export default async function BlogLayout({
   pageContent,
@@ -16,9 +16,9 @@ export default async function BlogLayout({
   const allPostsData = getSortedPostsData("content/posts/");
   return (
     <div>
-      <header className={header}>
-        <h1 className={headerTitle}>{title}</h1>
-        <h3 className={headerSubtitle}>{description}</h3>
+      <header className={styles.header}>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
       </header>
       <section>
         {contentHtml ? <SuperMarkdown markdownContent={contentHtml} /> : null}
