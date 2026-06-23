81c1b6adc0393d518624a6d7565d45f643556b1c
Ardi <ardillan@me.com>
2026-06-23 11:12:47 +0200

    fix: restore old config file

--- next.config.js
+++ next.config.js
@@ -4,15 +4,29 @@
 const path = require("path");
 
 const nextConfig = {
+  output: "standalone",
   turbopack: {
     root: __dirname,
   },
-
   images: {
     remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
     formats: ["image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  experimental: {
+    optimizePackageImports: ["react-syntax-highlighter", "react-markdown"],
+  },
+  outputFileTracingExcludes: {
+    "*": [
+      "node_modules/@swc/core-linux-x64-gnu",
+      "node_modules/@swc/core-linux-x64-musl",
+      "node_modules/@esbuild",
+      "node_modules/webpack",
+      "node_modules/rollup",
+      "node_modules/terser",
+      "_assets/**",
+    ],
   },
 };
 
