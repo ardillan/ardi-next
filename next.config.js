176acc7c1ce9b2b4cf048d1c1930de3a72d48765
Ardi <ardillan@me.com>
2026-06-23 11:09:12 +0200

    feat: update Next Config file

--- next.config.js
+++ next.config.js
@@ -7,8 +7,12 @@ const nextConfig = {
   turbopack: {
     root: __dirname,
   },
+
   images: {
     remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+    formats: ["image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
